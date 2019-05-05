5074f9c7294c6a7f9a27895fbc566d173dceb989
Hx <1020309534@qq.com>
2019-05-05 17:19:10 +0800

    update registerController

--- resources/views/users/auth/register.blade.php
+++ resources/views/users/auth/register.blade.php
@@ -63,34 +63,16 @@
             <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
                 <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="请输入邮箱">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-
-{{--                @if ($errors->has('email'))--}}
-{{--                    <span class="help-block">--}}
-{{--                        <strong>{{ $errors->first('email') }}</strong>--}}
-{{--                    </span>--}}
-{{--                @endif--}}
             </div>
 
             <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
                 <input type="password" class="form-control" name="password" placeholder="请输入密码">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-
-{{--                @if ($errors->has('password'))--}}
-{{--                    <span class="help-block">--}}
-{{--                        <strong>{{ $errors->first('password') }}</strong>--}}
-{{--                    </span>--}}
-{{--                @endif--}}
             </div>
 
             <div class="form-group has-feedback{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                 <input type="password" name="password_confirmation" class="form-control" placeholder="请确认密码">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-
-{{--                @if ($errors->has('password_confirmation'))--}}
-{{--                    <span class="help-block">--}}
-{{--                        <strong>{{ $errors->first('password_confirmation') }}</strong>--}}
-{{--                    </span>--}}
-{{--                @endif--}}
             </div>
 
             @include('common.error')
@@ -111,7 +93,7 @@
             </div>
         </form>
 
-        <a href="{{ url('/login') }}" class="text-center">I already have a membership</a>
+        <a href="{{ url('/login') }}" class="text-center">已有账号，去登录</a>
     </div>
     <!-- /.form-box -->
 </div>
@@ -121,9 +103,9 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <!-- AdminLTE App -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/js/adminlte.min.js"></script>
+<script src="{{ url('js/adminlte.min.js') }}"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
+<script src="{{ url('js/icheck.min.js') }}"></script>
 
 <script>
     $(function () {
