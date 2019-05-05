b62b5fbc9e257871b1d76b1ceab65d16927494f0
Hx <1020309534@qq.com>
2019-05-05 18:10:53 +0800

    update LoginController

--- app/AdminUser.php
+++ app/AdminUser.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class AdminUser extends Model
+class AdminUser extends Authenticatable
 {
     //
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'password'
+    ];
+
+    protected $hidden = ['password'];
+
 }
