2e9a794305c09010555063001ee635d2cb1fcc27
Hx <1020309534@qq.com>
2019-05-05 18:20:53 +0800

    update LoginController

--- app/Http/Controllers/Users/LoginController.php
+++ app/Http/Controllers/Users/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
         // 逻辑
         $user = $request->only('name', 'password');
 //        $remember = boolval($request->input('remember'));
-        if(\Authguard('web')->attempt($user)){
+        if(\Auth::guard('web')->attempt($user)){
             return redirect('/home');
         }
 
