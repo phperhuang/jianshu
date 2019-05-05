03e5e91f4e6b80eb56fa0e3679504698a6fbc3f7
Hx <1020309534@qq.com>
2019-05-05 19:23:11 +0800

    update post.create.blade

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="blog-footer">
+<footer style="align-content: center" class="blog-footer">
     <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
     <p>
         <a href="#">Back to top</a>
