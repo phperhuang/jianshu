88cb0d0570d9641c8856d78db8f532693ae68046
Hx <1020309534@qq.com>
2019-05-06 19:05:36 +0800

    delete footer

--- app/Post.php
+++ app/Post.php
@@ -12,19 +12,19 @@ class Post extends Model
     // 作者
     public function user()
     {
-        return $this->belongsTo('App\User', 'user_id', 'id');
+        return $this->belongsTo(\App\User::class, 'user_id', 'id');
     }
 
     // 赞
     public function zans()
     {
-        return $this->hasMany('App\Zan')->orderBy('created_at', 'desc');
+        return $this->hasMany(\App\Zan::class)->orderBy('created_at', 'desc');
     }
 
     // 评论
     public function comments()
     {
-        return $this->hasMany('App\Comment')->orderBy('created_at', 'desc');
+        return $this->hasMany(\App\Comment::class)->orderBy('created_at', 'desc');
     }
 
 }
