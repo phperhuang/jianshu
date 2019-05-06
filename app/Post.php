1e8ca9b91f113667e540bad140f952959ca3f4ff
Hx <1020309534@qq.com>
2019-05-06 10:02:48 +0800

    update PostController@index

--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,11 @@ class Post extends Model
     protected $table = 'posts';
 
     protected $fillable = ['title', 'content', 'user_id'];
+
+    // 作者
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
 }
